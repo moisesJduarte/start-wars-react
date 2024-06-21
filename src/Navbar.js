0870c875b5d46807e80d9ceaba507443e1806a78
moisesJduarte <gladmd19@gmail.com>
2024-06-21 14:34:56 -0400

    adding MediaQuery

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
-import logo from './star-wars-logo.png'
+import logo from './star-wars-logo.png';
 
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
-        <div className="sidebar">
-            <div className="sidebar-logo">
-                <img src={logo} alt="Star Wars Logo" />
+        <>
+            <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+                <div className="sidebar-logo">
+                    <img src={logo} alt="Star Wars Logo" />
+                </div>
+                <h2>Navigation</h2>
+                <a href="" className="sidebar-link">Home</a>
+                <a href="#" className="sidebar-link">Detail</a>
+                <a href="#" className="sidebar-link">Profile</a>
+                <div className="sidebar-footer">
+                    Created by <a href="https://www.linkedin.com/in/moises-duarte-944b26209/" target="_blank" rel="noopener noreferrer">Moises</a>
+                </div>
             </div>
-            <h2>Navigation</h2>
-            <a href="" className="sidebar-link">Home</a>
-            <a href="#" className="sidebar-link">Detail</a>
-            <a href="#" className="sidebar-link">Profile</a>
-            <div className="sidebar-footer">
-                Created by <a href="https://www.linkedin.com/in/moises-duarte-944b26209/" target="_blank" rel="noopener noreferrer">Moises</a>
-            </div>
-        </div>
+            <button className="sidebar-toggle" onClick={toggleSidebar}>
+                {isOpen ? 'Hide Sidebar' : 'Show Sidebar'}
+            </button>
+        </>
     );
 };
 
