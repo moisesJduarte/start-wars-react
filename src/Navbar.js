6aa95d3f8a2c075bea236bedbac62c06b5138536
moisesJduarte <gladmd19@gmail.com>
2024-06-20 20:49:13 -0400

    updating sidebar

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,9 +9,12 @@ const NavBar = () => {
                 <img src={logo} alt="Star Wars Logo" />
             </div>
             <h2>Navigation</h2>
-            <a href="#" className="sidebar-link">Home</a>
+            <a href="" className="sidebar-link">Home</a>
             <a href="#" className="sidebar-link">Detail</a>
             <a href="#" className="sidebar-link">Profile</a>
+            <div className="sidebar-footer">
+                Creado por <a href="https://www.linkedin.com/in/moises-duarte-944b26209/" target="_blank" rel="noopener noreferrer">Moises</a>
+            </div>
         </div>
     );
 };
