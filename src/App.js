36d2c1bde6941c51f575e472d5f89337e545253e
moisesJduarte <gladmd19@gmail.com>
2024-06-21 23:55:18 -0400

    path  router

--- src/App.js
+++ src/App.js
@@ -3,23 +3,24 @@ import './App.css';
 import StarWars from './components/StarWars';
 import NavBar from './components/nav/Navbar';
 import Detail from './components/detail/detail';
+import { Route, Routes } from 'react-router-dom';
 // import Counter from './Counter';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar />
-        <StarWars />
-        <Detail />
-        {/* <Counter /> */}
 
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<StarWars />} />
+          <Route path="/detail" element={<Detail />} />
+        </Routes>
+
+        {/* <Counter /> */}
       </header>
     </div>
   );
 }
 
 export default App;
-
-
--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Navbar.css';
 import logo from './star-wars-logo.png';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -16,9 +17,10 @@ const NavBar = () => {
                     <img src={logo} alt="Star Wars Logo" />
                 </div>
                 <h2>Navigation</h2>
-                <a href="" className="sidebar-link">Home</a>
-                <a href="#" className="sidebar-link">Detail</a>
-                <a href="#" className="sidebar-link">Profile</a>
+                <Link to="/" className="sidebar-link">Home</Link>
+                <Link to="/detail" className="sidebar-link">Detail</Link>
+                <Link to="/profile" className="sidebar-link">Profile</Link>
+
                 <div className="sidebar-footer">
                     Created by <a href="https://www.linkedin.com/in/moises-duarte-944b26209/" target="_blank" rel="noopener noreferrer">Moises</a>
                 </div>
